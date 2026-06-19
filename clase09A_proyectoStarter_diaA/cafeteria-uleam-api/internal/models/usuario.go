e21796d8d4cae58f8f349ab61c32d8c1b930b5a6
Bladerm <josemanuelcastilloloor@gmail.com>
2026-06-19 17:28:54 -0500

    token

--- clase09A_proyectoStarter_diaA/cafeteria-uleam-api/internal/models/usuario.go
+++ clase09A_proyectoStarter_diaA/cafeteria-uleam-api/internal/models/usuario.go
@@ -4,7 +4,7 @@ import "time"
 
 type Usuario struct {
 	ID           int       `json:"id" gorm:"primaryKey"`
-	Email        string    `json:"email" gorm:"not null;uniqueIndex"`
-	PasswordHash string    `json:"-" gorm:"not null"`
-	CreatedAt    time.Time `json:"creado_en"`
+	Email        string    `json:"email" gorm:"unique;not null"`
+	PasswordHash string    `json:"password" gorm:"not null"`
+	CreadoEn     time.Time `json:"creado_en"`
 }
